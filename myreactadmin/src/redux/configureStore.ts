fac820a0aef098b39a25033cd041d14ec570dd1d
Taihocmot <lephattaihotmot@gmail.com>
2023-03-28 11:11:16 +0700

    remove myreactadmin

--- myreactadmin/src/redux/configureStore.ts
+++
@@ -1,8 +0,0 @@
-import {legacy_createStore as createStore} from 'redux'
-import setUserReducer from './reducers/setUserReducer'
-
-const configureStore = () => {
-    return createStore(setUserReducer);
-}
-
-export default configureStore;
--- myreactadmin/src/secure/NavigateToDashboard.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const NavigateToDashboard = () => <Navigate replace to = '/dashboard'/>
-
-export default NavigateToDashboard;
